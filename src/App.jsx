680e7842a28e5b12ee159844e6d755efe256a2da
Bernardthe3rd <bammeijer@outlook.com>
2024-03-03 18:16:55 +0100

    started assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import './App.css';
+import axios from "axios";
+import {useState} from "react";
+// import {region} from "./helpers/region.js";
 
 function App() {
+    //stukje state
+    const [countries, getCountries] = useState([]);
+
+    //de async functie
+    async function fetchCountries () {
+        try {
+            const response = await axios.get('https://restcountries.com/v3.1/all')
+            console.log(response.data[4])
+            getCountries(response.data)
+        } catch(e) {
+            console.log(e)
+        }
+    }
 
     return (
         <>
-            Maak je applicatie hier!
+            <h1>Kleine titel om overzicht te maken</h1>
+            <button onClick={fetchCountries}>
+                Click on me!
+            </button>
+            <ul>
+                {countries.map((country) => {
+                   return <li key={country.name.common}>
+                       <img src={country.flags.png} alt={country.flags.alt}/>
+                       <h4>{country.name.common}</h4>
+                       <p>Has a population of {country.population} people</p>
+                   </li>
+                })}
+            </ul>
         </>
     )
 }
---
+++ src/helpers/region.js
@@ -0,0 +1,8 @@
+
+export const region = (country) => {
+    if (country.region === "Europe") {
+        return "europe"
+    } else {
+        return "anywhere"
+    }
+}
